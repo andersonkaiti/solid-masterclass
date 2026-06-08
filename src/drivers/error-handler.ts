d3487258697035ac1aa90aaa9b2c3385f1b8eaa0
Anderson Kaiti <anderkaiti@gmail.com>
2026-06-08 15:17:02 -0300

    :bug: fix(drivers): handle Zod schema validation errors in global error handler

--- src/drivers/error-handler.ts
+++ src/drivers/error-handler.ts
@@ -1,4 +1,5 @@
 import type { FastifyReply, FastifyRequest } from 'fastify'
+import { hasZodFastifySchemaValidationErrors } from 'fastify-type-provider-zod'
 import { EmailAlreadyExistsError } from '../application/errors/email-already-exists.ts'
 import { PasswordsDoNotMatchError } from '../application/errors/passwords-do-not-match.ts'
 import { UserCreationError } from '../application/errors/user-creation.ts'
@@ -8,6 +9,12 @@ export function errorHandler(
   _request: FastifyRequest,
   reply: FastifyReply,
 ) {
+  if (hasZodFastifySchemaValidationErrors(error)) {
+    return reply.status(400).send({
+      error: error.message,
+    })
+  }
+
   if (error instanceof PasswordsDoNotMatchError) {
     return reply.status(400).send({
       error: error.message,
